8df9193ace3ecc0da84425f5593e152c0e6ca3c9
Ekaterina Petushova <epetushova@gmail.com>
2017-02-20 08:19:34 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,9 +6,16 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @post.to_json(only: [:title, :description, :id],
+                              include: [author: { only: [:name]}]) }
+    end
   end
 
   def new
+    @message = "Submit Post"
     @post = Post.new
   end
 
@@ -19,6 +26,7 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @message = "Update Post"
   end
 
   def update
@@ -28,7 +36,9 @@ class PostsController < ApplicationController
 
   def post_data
     post = Post.find(params[:id])
-    render json: PostSerializer.serialize(post)
+    #render json: PostSerializer.serialize(post)
+    render json: post.to_json(only: [:title, :description, :id],
+                              include: [ author: { only: [:name]}])
   end
 
 private
